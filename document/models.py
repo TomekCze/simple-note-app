5b806eebfd8ccc02eacbdbefdebf1284b169678a
TomekCze <tomek.czernecki@gmail.com>
2020-09-01 23:20:32 +0200

    Add document, view, template

---
+++ document/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
---
+++ document/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Document
+
+
+def editor(request):
+    docid = int(request.GET.get('docid', 0))
+    documents = Document.objects.all()
+
+    context = {
+        'docid': docid,
+        'documents': documents
+    }
+
+    return render(request, 'editor.html', context)
